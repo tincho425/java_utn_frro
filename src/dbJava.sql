e54adc75ceb8533542d5b6a0601e3e224d42b0cc
Martín Ballestero <tincho.ballestero@gmail.com>
2021-11-28 23:54:08 -0300

    Cliente & Llamada & Servicio DB.

--- src/dbJava.sql
+++ src/dbJava.sql
@@ -26,13 +26,13 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `callcenter` /*!40100 DEFAULT CHARACTER
 USE `callcenter`;
 
 --
--- Table structure for table `persona`
+-- Table structure for table `usuario`
 --
 
-DROP TABLE IF EXISTS `persona`;
+DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `persona` (
+CREATE TABLE `usuario` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tipo_doc` varchar(10) NOT NULL,
   `nro_doc` varchar(45) NOT NULL,
@@ -48,14 +48,44 @@ CREATE TABLE `persona` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `persona`
+-- Dumping data for table `usuario`
 --
 
-LOCK TABLES `persona` WRITE;
-/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
-INSERT INTO `persona` VALUES (1,'dni','10101010','Juan','Perez','jp@gmail.com','4101010',1,'jperez',DEFAULT),(2,'dni','12121212','John','Doe','contacto@jd','4121212',0,'jdoe',DEFAULT),(3,'dni','13131313','Nadie','Sabe','ns@ns.com','4131313',1,'nsabe',DEFAULT),(4,'cuit','14141414141','Identidad','Desconocida','unknown@gmail.com','4141414',0,'idesconocida',DEFAULT),(5,'cuit','15151515151','Alguien','Más','am@gmail.com','4151515',1,'amas',DEFAULT),(6,'dni','16161616','Otra','Persona','op@gmail.com','4161616',0,'opersona',DEFAULT);
-/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'dni','10101010','Juan','Perez','jp@gmail.com','4101010',1,'jperez',DEFAULT),(2,'dni','12121212','John','Doe','contacto@jd','4121212',0,'jdoe',DEFAULT),(3,'dni','13131313','Nadie','Sabe','ns@ns.com','4131313',1,'nsabe',DEFAULT),(4,'cuit','14141414141','Identidad','Desconocida','unknown@gmail.com','4141414',0,'idesconocida',DEFAULT),(5,'cuit','15151515151','Alguien','Más','am@gmail.com','4151515',1,'amas',DEFAULT),(6,'dni','16161616','Otra','Persona','op@gmail.com','4161616',0,'opersona',DEFAULT);
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
+
+
+--
+-- Table structure for table `cliente`
+--
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cliente` (
+	`dni` INT(8) NOT NULL,
+	`apellido` varchar(255) NOT NULL,
+	`nombre` varchar(255) NOT NULL,
+	`CUIT` INT(11) NOT NULL,
+	`telefono` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	PRIMARY KEY (`dni`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (39120610,'ballestero','martin',20391206105,'+543416295949','martin.ballestero@hotmail.com'),(39120611,'perez','ricardo',20391206115,'+543416295959','perez.ricardo@hotmail.com');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
 
 --
 -- Table structure for table `rol`
@@ -82,31 +112,90 @@ INSERT INTO `rol` VALUES (1,'admin'),(2,'agent');
 UNLOCK TABLES;
 
 --
--- Table structure for table `rol_persona`
+-- Table structure for table `rol_usuario`
 --
 
-DROP TABLE IF EXISTS `rol_persona`;
+DROP TABLE IF EXISTS `rol_usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `rol_persona` (
-  `id_persona` int(11) NOT NULL,
+CREATE TABLE `rol_usuario` (
+  `id_usuario` int(11) NOT NULL,
   `id_rol` int(11) NOT NULL,
-  PRIMARY KEY (`id_persona`,`id_rol`),
-  KEY `rol_persona_rol_fk` (`id_rol`),
-  CONSTRAINT `rol_persona_persona_fk` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id`),
-  CONSTRAINT `rol_persona_rol_fk` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id`)
+  PRIMARY KEY (`id_usuario`,`id_rol`),
+  KEY `rol_usuario_rol_fk` (`id_rol`),
+  CONSTRAINT `rol_usuario_usuario_fk` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
+  CONSTRAINT `rol_usuario_rol_fk` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `rol_persona`
+-- Dumping data for table `rol_usuario`
 --
 
-LOCK TABLES `rol_persona` WRITE;
-/*!40000 ALTER TABLE `rol_persona` DISABLE KEYS */;
-INSERT INTO `rol_persona` VALUES (1,1),(2,2),(3,1),(3,2),(4,2),(5,2),(6,2);
-/*!40000 ALTER TABLE `rol_persona` ENABLE KEYS */;
+LOCK TABLES `rol_usuario` WRITE;
+/*!40000 ALTER TABLE `rol_usuario` DISABLE KEYS */;
+INSERT INTO `rol_usuario` VALUES (1,1),(2,2),(3,1),(3,2),(4,2),(5,2),(6,2);
+/*!40000 ALTER TABLE `rol_usuario` ENABLE KEYS */;
 UNLOCK TABLES;
+
+
+
+--
+-- Table structure for table `llamada`
+--
+DROP TABLE IF EXISTS `llamada`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `llamada` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`timestamp_created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`timestamp_ended` TIMESTAMP NULL DEFAULT NULL,
+	`id_usuario` int(11) NOT NULL,
+	`id_cliente` INT(8) DEFAULT NULL,
+	PRIMARY KEY (`id`),
+	CONSTRAINT `llamada_usuario_fk` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
+	CONSTRAINT `llamada_cliente_fk` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`dni`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `llamada`
+--
+
+LOCK TABLES `llamada` WRITE;
+/*!40000 ALTER TABLE `llamada` DISABLE KEYS */;
+INSERT INTO `llamada` (id_usuario) VALUES (1);
+/*!40000 ALTER TABLE `llamada` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- Table structure for table `servicio`
+--
+DROP TABLE IF EXISTS `servicio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `servicio` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`nombre` varchar(255) NOT NULL,
+	`descripcion` varchar(255) DEFAULT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicio`
+--
+
+LOCK TABLES `servicio` WRITE;
+/*!40000 ALTER TABLE `servicio` DISABLE KEYS */;
+INSERT INTO `servicio` (nombre, descripcion) VALUES ("Fibra óptica - 100MB","El servicio brinda acceso a internet con velocidades de subida y bajada paralelas de 100MB"),
+("Fibra óptica - 200MB", "El servicio brinda acceso a internet con velocidades de subida y bajada paralelas de 200MB");
+/*!40000 ALTER TABLE `servicio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
